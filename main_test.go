400cf47ff1fee44ad445eb9c090783d407689be2
Gustavo Inácio <gsinacio94@gmail.com>
2024-07-08 01:15:05 -0300

    1: Structs in Go

---
+++ main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gustavosinacio/bootdev-go/structs"
+)
+
+func getMessageText(m structs.MessageToSend) string {
+	return fmt.Sprintf("Sending message: '%s' to: %v\n", m.Message, m.PhoneNumber)
+}
+
+func Test(t *testing.T) {
+	type testCase struct {
+		message     string
+		expected    string
+		phoneNumber int
+	}
+	tests := []testCase{
+		{
+			"Thanks for signing up",
+			"Sending message: 'Thanks for signing up' to: 148255510981\n",
+			148255510981,
+		},
+		{
+			"Love to have you aboard!",
+			"Sending message: 'Love to have you aboard!' to: 148255510982\n",
+			148255510982,
+		},
+	}
+	if withSubmit {
+		tests = append(tests, []testCase{
+			{
+				"We're so excited to have you",
+				"Sending message: 'We're so excited to have you' to: 148255510983\n",
+				148255510983,
+			},
+			{
+				"",
+				"Sending message: '' to: 148255510984\n",
+				148255510984,
+			},
+			{
+				"Hello, World!",
+				"Sending message: 'Hello, World!' to: 148255510985\n",
+				148255510985,
+			},
+			{
+				"This is a test message",
+				"Sending message: 'This is a test message' to: 148255510986\n",
+				148255510986,
+			},
+		}...)
+	}
+
+	for _, test := range tests {
+		if output := getMessageText(structs.MessageToSend{
+			PhoneNumber: test.phoneNumber,
+			Message:     test.message,
+		}); output != test.expected {
+			t.Errorf(
+				"Test Failed: (%v, %v) -> expected: %v actual: %v",
+				test.phoneNumber,
+				test.message,
+				test.expected,
+				output,
+			)
+		} else {
+			fmt.Printf("Test Passed: (%v, %v) -> expected: %v actual: %v\n",
+				test.phoneNumber,
+				test.message,
+				test.expected,
+				output,
+			)
+		}
+	}
+}
+
+// withSubmit is set at compile time depending
+// on which button is used to run the tests
+var withSubmit = true
---
+++ structs/messageToSend.go
@@ -0,0 +1,6 @@
+package structs
+
+type MessageToSend struct {
+	Message     string
+	PhoneNumber int
+}
